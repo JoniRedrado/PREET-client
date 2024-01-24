39cbb85a78a5640c58bd5e04e351263e246ea528
JuanDYanez <juancho644@gmail.com>
2024-01-24 04:36:25 -0500

    :sparkles: Pagination component
    Pagination component in Cards

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux";
+import { nextPage, prevPage, specificPage } from "../../redux/actions";
+
+import s from "./pagination.module.css";
+
+function Pagination({hotels, hotelsPerPage}) {
+
+  const dispatch = useDispatch()
+
+  const pageNumbers = [];
+  const currentPage = useSelector((state) => state.currentPage)
+
+  for(let i = 1;i<=Math.ceil(hotels.length/hotelsPerPage); i++){
+    pageNumbers.push(i)
+  }
+
+  const onNextPage = () => {
+    dispatch(nextPage())
+  }
+
+  const onPrevPage = () => {
+    dispatch(prevPage())
+  }
+
+  const onSpecificPage = (page) => {
+    dispatch(specificPage(page))
+  }
+
+  const showPageNumbers = () => {
+    const displayPages = []
+    const totalPages = pageNumbers.length
+    const pagesToShow = 3;
+
+    console.log(totalPages);
+
+    if (totalPages <= pagesToShow) {
+      displayPages.push(...pageNumbers)
+    } else {
+      const startPage = Math.max(1, currentPage)
+      const endPage = Math.min(startPage + pagesToShow - 1, totalPages);
+
+      if (startPage > 1) {
+        displayPages.push(1);
+        displayPages.push(null)
+      }
+
+      for (let i = startPage; i <= endPage; i++) {
+        displayPages.push(i)
+      }
+
+      if (endPage < totalPages) {
+        displayPages.push(null)
+        displayPages.push(totalPages)
+      }
+    }
+
+    return displayPages.map((page, index) => (
+      <li key={index}>
+        {page === null
+          ? (<span>   </span>)
+          : (<a onClick={() => onSpecificPage(page)}>{page}</a>)
+        }
+      </li>
+    ))
+  }
+  
+  return (
+    <div className={s.pagination}>
+      <button onClick={onPrevPage}>Anterior</button>
+
+      <div>{showPageNumbers()}</div>
+
+      <button onClick={onNextPage}>Siguiente</button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,31 @@
-import {
-  COUNTER
-} from "./actions-types";
+import { NEXT_PAGE, PREV_PAGE, SPECIFIC_PAGE, SET_CURRENT_PAGE } from "./actions-types";
 
 let initialState = {
-  counter: 1
+  currentPage: 1
 };
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
-    case COUNTER:
+    case NEXT_PAGE:
       return {
         ...state,
-        counter: action.payload
-      }
+        currentPage: action.payload,
+      };
+    case PREV_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload,
+      };
+    case SPECIFIC_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload,
+      };
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.payload,
+      };
     default:
       return state;
   }
