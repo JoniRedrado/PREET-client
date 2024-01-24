d2155874f28f70536580169b6976f1591f55b6f7
FFRavassi <francescoravassi@gmail.com>
2024-01-23 22:16:46 -0300

    NavBar c/boton create

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Detail from './Pages/Detail/Detail'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Detail from "./Pages/Detail/Detail";
+import NavBar from "./Pages/NavBar/NavBar";
 
 // import './App.css'
 
 function App() {
-
-
   return (
-    
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/:id' element={<Detail/>}/>
-      {/* <Route path='/create' element={<Create/>}/> */}
-    </Routes>
-  )
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+        {/* <Route path='/create' element={<Create/>}/> */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
