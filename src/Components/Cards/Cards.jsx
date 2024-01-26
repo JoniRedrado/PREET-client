0823b4b4621fbd0f9789693e04889375d437ae8f
DarwingDiaz <darwingdiazsanchez@gmail.com>
2024-01-26 15:02:52 -0500

    Cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -12,9 +12,10 @@ const Cards = ({ allHotels }) => {
           key={hotel.id}
           id={hotel.id}
           name={hotel.name} 
-          address={hotel.address}
-          country={hotel.countryId} 
-          address_url={hotel.address_url} 
+          // address={hotel.address}
+          country={hotel.country.name} 
+          // address_url={hotel.address_url}
+          stars={hotel.stars}
           price={hotel.price} 
           email={hotel.email} 
           image={hotel.image} 
