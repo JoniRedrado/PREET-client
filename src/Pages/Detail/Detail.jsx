636a7b1f22a87f49bd8b295f90e728f29fc89af9
JuanDYanez <juancho644@gmail.com>
2024-01-31 14:55:50 -0500

    :zap: Se configura la visual del home con sesión cerrada
    Si no hay una sesión iniciada se mostrarán todos los hoteles y filtros, y no dejará actualizar o borrar. Al iniciar sesión ya permitirá crear hoteles, actualizarlos o borrarlos.

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -8,6 +8,8 @@ import "./detail.styles.css"
 
 const Detail = () =>{
 
+  const token = localStorage.getItem('token')
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -28,33 +30,41 @@ const Detail = () =>{
 
   const hotel = useSelector((state) => state.hotelDetail)
   console.log(hotel.country)
-  return(
-    <div className="container">
-      <div>
-        <Link to="/">
-        <i className="bi bi-arrow-left-circle" title="Return home "></i>
-        </Link>
-        <Link to= {`/update/${hotel.id}`}>
-            <i class="bi bi-pencil-square" title="Update"></i>
-        </Link>
-        <i class="bi bi-trash" onClick={() => handleDelete(id)} title="Delete"></i>  
-      </div>
-       
-      {hotel ? (
+  return (
+    <>
+      <div className="container">
         <div>
-      <h1>{hotel.name}</h1>
-      <img src={hotel.image} alt={hotel.name}/>
-      <h2>{hotel.stars}⭐</h2>
-      <h2>Price per night {hotel.price} $</h2>
-      <h2>Address {hotel.address}</h2>
-      <h2>Country {hotel.country && hotel.country.name}</h2>
-      <h2>Ubication {hotel.address_url}</h2>
-      <h2>Contact {hotel.email}</h2>
-      </div> ) : (
-        <p>cargando...</p>
-      )}
-       
-    </div>
+          <Link to="/">
+          <i className="bi bi-arrow-left-circle" title="Return home "></i>
+          </Link>
+        { token ?
+            (
+              <>
+          <Link to= {`/update/${hotel.id}`}>
+              <i class="bi bi-pencil-square" title="Update"></i>
+          </Link>
+          <i class="bi bi-trash" onClick={() => handleDelete(id)} title="Delete"></i>  
+              </>    
+          ) :
+              ""
+        }
+        </div>
+         
+        {hotel ? (
+          <div>
+        <h1>{hotel.name}</h1>
+        <img src={hotel.image} alt={hotel.name}/>
+        <h2>{hotel.stars}⭐</h2>
+        <h2>Price per night {hotel.price} $</h2>
+        <h2>Address {hotel.address}</h2>
+        <h2>Country {hotel.country && hotel.country.name}</h2>
+        <h2>Ubication {hotel.address_url}</h2>
+        <h2>Contact {hotel.email}</h2>
+        </div> ) : (
+          <p>cargando...</p>
+        )}
+      </div>
+    </>
 
   )
 
