4f05537f45bf2f5cc956385e9682aec0017c9e74
DarwingDiaz <darwingdiazsanchez@gmail.com>
2024-01-31 09:33:13 -0500

    pending-ok

--- src/Components/Try again/TryAgain.jsx
+++ src/Components/Try again/TryAgain.jsx
@@ -4,8 +4,8 @@ import "./TryAgain.styles.css"
 const tryAgain = () => {
     return(
         <div className="conLoa">
-            <i class="bi bi-chat-square-text"></i>
-            <h1 className="not">The search did not yield any results, please enter the name of another hotel</h1>
+            <i class="bi bi-chat-square-text icon-mesagge"></i>
+            <h1 className="not">The hotel search you conducted did not yield any results. Please try again.</h1>
         </div>
     )
 }
