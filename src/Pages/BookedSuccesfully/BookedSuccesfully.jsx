81ad94c1b52ea923896670a65e8033e1b1b631cc
millanjuan <millanj159951@gmail.com>
2024-02-19 00:58:51 -0300

    booking details & success page ready

--- src/Pages/BookedSuccesfully/BookedSuccesfully.jsx
+++ src/Pages/BookedSuccesfully/BookedSuccesfully.jsx
@@ -1,30 +1,52 @@
-import { useEffect } from 'react'
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react'
 import axios from 'axios'
-import './BookedSuccesfully.styles.css'
-
-
+import Loading from '../../assets/Loading.gif'
+import { FaCheckCircle } from 'react-icons/fa';
+import styles from "./BookedSuccesfully.module.css";
+import { useNavigate } from 'react-router-dom';
 
 const BookedSuccesfully = () => {
-    //Paypal te devuelve a tu app con un token por params, enviamos ese token al back para que confirme que la reserva fue pagada
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const token = queryParams.get("token");
+    const navigate = useNavigate();
+    const [booking, setBooking] = useState(null)
+    const getUserReservations = async () => {
+      try {
+        const { data } = await axios.get(`${import.meta.env.VITE_BACK_URL}/bookings/last`);
+          setBooking(data)
+          console.log(booking)
+      } catch (error) {
+          console.error(error.message)
+      }
+  }
 
-    useEffect(() => {
-        axios.get(`${import.meta.env.VITE_BACK_URL}/payment/capture-order?token=${token}`)
-            .then(res=>{
-                console.log(res.data);
-            })
-    })
+  useEffect(() => {
+      getUserReservations()
+  }, [])
+
+  if (!booking) {
+    return <div className={styles.loading}>{Loading}</div>;
+  }
+
+  const handleClick = () => {
+    navigate(`/bookingDetails/${booking.id}`)
+  };
 
   return (
-    <div className="main-booked-container">
-      <i className='bi bi-calendar2-check' title='booked-success'></i>
-      <h1>¡Your room has been successfully booked!</h1>
-      <button onClick={() => (window.location.href = "/")}>Return Home</button>
+    <div className={styles.mainContainer}>
+      <FaCheckCircle className={`${styles.icon} ${styles.tickIcon}`} />
+    <h2 className={styles.title}>¡Booking Succesfully Completed!</h2>
+    <div className={styles.bookingInfo}>
+        <h2>Booking Information</h2>
+        <p><strong>Name:</strong> {booking.user.name} {booking.user.last_name}</p>
+        <p><strong>Email:</strong> {booking.user.email}</p>
+        <p><strong>Amount:</strong> ${booking.amount}</p>
+        <p><strong>Date Init:</strong> {new Date(booking.dateInit).toLocaleDateString()}</p>
+        <p><strong>Date Final:</strong> {new Date(booking.dateFinal).toLocaleDateString()}</p>
+        <p><strong>Nights:</strong> {booking.nights}</p>
+        <p><strong>Payment Reference:</strong> {booking.pay}</p>
     </div>
-  );
+    <button className={styles.button} onClick={handleClick}>Booking Details</button>
+  </div>
+);
 }
 
-export default BookedSuccesfully
+export default BookedSuccesfully;
